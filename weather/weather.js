fd812c55d8583ee64bfa0ff5cfd06cfdcbb0ff1c
Abhinav Mishra <mishraabhinav2306@gmail.com>
2023-12-18 02:53:10 +0530

    added exception handling

--- weather/weather.js
+++ weather/weather.js
@@ -6,11 +6,18 @@
     const weatherIcon = document.getElementById("weather-icon");
     
     
-    async function checkWeather(city) {
-        const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
+async function checkWeather(city) {
+
+    try {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        
+        if (response.status == 404) {
+            throw new error('city not found');
+        }
         var data = await response.json();
 
         console.log(data);
+        
 
         document.querySelector(".city-name").innerHTML = data.name;
         document.querySelector(".temperature").innerHTML = data.main.temp + "°C";
@@ -18,31 +25,53 @@
         document.querySelector(".wind-speed").innerHTML = data.wind.speed + " km/h <br> wind speed";
 
         if (data.weather[0].main === "Clear") {
-            weatherIcon.src="clear.png";
+            weatherIcon.src = "clear.png";
         }
         else if (data.weather[0].main === "Clouds") {
-            weatherIcon.src="clouds.png";
+            weatherIcon.src = "clouds.png";
         }
         else if (data.weather[0].main === "Mist") {
-            weatherIcon.src="mist.png";
+            weatherIcon.src = "mist.png";
         }
         else if (data.weather[0].main === "Rain") {
-            weatherIcon.src="rain.png";
+            weatherIcon.src = "rain.png";
         }
         else if (data.weather[0].main === "Snow") {
-            weatherIcon.src="snow.png";
+            weatherIcon.src = "snow.png";
         }
         else if (data.weather[0].main === "Drizzle") {
-            weatherIcon.src="drizzle.png";
+            weatherIcon.src = "drizzle.png";
         }
         else if (data.weather[0].main === "Smoke") {
-            weatherIcon.src="smoke.png";
+            weatherIcon.src = "smoke.png";
         }
         else if (data.weather[0].main === "Haze") {
-            weatherIcon.src="haze.png";
+            weatherIcon.src = "haze.png";
         }
+    }
+
+    catch (error) {
+        console.log(error);
+        document.querySelector(".error-msg").style.display = "block";
+        document.querySelector(".temperature-details").style.display = "none";
+        document.querySelector(".extra-details").style.display = "none";
+
+        searchBtn.addEventListener("click", () => {
+        checkWeather(searchBox.value);
+        })
+        document.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+        checkWeather(searchBox.value);
+        }
+        })
+    }
     }
 
 searchBtn.addEventListener("click", () => {
 checkWeather(searchBox.value);
+})
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+checkWeather(searchBox.value);
+}
 })
